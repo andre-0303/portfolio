0ab29331a24b31bc58d60ae38ba3adb7d0caa4df
andre-0303 <bandeiradev.ti@gmail.com>
2025-02-26 18:58:00 -0300

    alteracao no h1

--- scripts.js
+++ scripts.js
@@ -13,3 +13,32 @@ function enviarWhatsApp(event) {
     window.open(url, '_blank');
 
 }
+
+const txtEfeito = document.querySelector(".txtEfeito");
+const alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let intervalo = null;
+
+if (txtEfeito) {
+    txtEfeito.addEventListener("mouseover", (e) => {
+        let contador = 0;
+        clearInterval(intervalo);
+
+        intervalo = setInterval(() => {
+            e.target.innerText = e.target.innerText
+                .split("")
+                .map((letter, index) => {
+                    return index < contador
+                        ? e.target.dataset.value[index]
+                        : alfabeto[Math.floor(Math.random() * 26)];
+                })
+                .join("");
+
+            if (contador >= e.target.dataset.value.length) {
+                clearInterval(intervalo);
+            }
+
+            contador += 1 / 3;
+        }, 30);
+    });
+}
